4e36a0757f524506a1a6f1f5a6ddcf22b3c27ebe
busra6335 <bpolat6335@gmail.com>
2022-10-31 22:52:20 +0300

    Öğrenci Classı ikinci bölüm yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,9 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("Merhaba GitHub");
+
+
+        System.out.println("Merhaba GitHub2");
+        System.out.println("Merhaba GitHub3");
+
     }
 }
